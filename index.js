2a02eb7f511191c8e776080a7e788dcdd154db5e
parazitexe <chobak789>
2021-04-04 21:54:03 +0300

    Upload Files https://www.w3schools.com/nodejs/nodejs_uploadfiles.asp

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var date = require('./date');
 var path = require('path');
 var fs = require('fs');
 var url = require('url');
+var events = require('events');
+var formidable = require('formidable');
+var mv = require('mv');
+
+
 
 /*http.createServer(function (req, res) {
     console.log(req.headers.cookie);
@@ -42,18 +47,48 @@ var url = require('url');
     console.log('File deleted!');
 });*/
 
-var filinrs = fs.createReadStream('./filin.jpg');
-filinrs.close();
-
-filinrs.on('open', function () {
-    console.log('The file is open');
-});
-filinrs.on('close', function () {
-    console.log('The file is closed');
-});
 
 
+/*var eventEmitter = new events.EventEmitter();
 
+//Create an event handler:
+var fuilinHandler = function () {
+    console.log('I hear a filin!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('filin', fuilinHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('filin');*/
+
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            var oldpath = files.filetoupload.path;
+
+            var newpath = path.resolve(__dirname, 'files', files.filetoupload.name);
+            console.log(oldpath);
+            console.log(newpath);
+            mv(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+            });
+        });
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+    }
+}).listen(8080);
 
 
 
