74398e3bb581529ec61e91cf90eb35a0e532fb8a
parazitexe <chobak789>
2021-04-09 00:23:41 +0300

    Node.js MongoDB Update https://www.w3schools.com/nodejs/nodejs_mongodb_update.asp

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # node-w3schools
-Node.js MongoDB Query
-https://www.w3schools.com/nodejs/nodejs_mongodb_query.asp
+Node.js MongoDB Update
+https://www.w3schools.com/nodejs/nodejs_mongodb_update.asp
 
--- index.js
+++ index.js
@@ -5,17 +5,41 @@ MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
 
-/*    dbo.collection("clients").insertOne({ name: "Vasia filin",address: "filin street 42" , rank: "filin" }, function(err, res) {
+    var filins = db.db("filins");
+
+/*    filins.createCollection("filins-list3", function(err, res) {
         if (err) throw err;
-        console.log("1 document inserted");
+        console.log("Collection created!");
+        db.close();
+    });
+    dbo.createCollection("filins3", function(err, res) {
+        if (err) throw err;
+        console.log("Collection created!");
         db.close();
     });*/
 
-    dbo.collection("clients").find({ name: "Vasia filin" }, { projection: { _id: 0, rank: 1} }).toArray(function(err, result) {
+    dbo.dropCollection("filins", function(err, delOK) {
+        if (err) throw err;
+        if (delOK) console.log("Collection deleted");
+        db.close();
+    })
+
+/*    dbo.collection("customers").deleteMany({ }, function(err, obj) {
+        if (err) throw err;
+        console.log("1 document deleted");
+        db.close();
+    });*/
+
+
+
+/*    dbo.collection("clients").find({ }, { projection: { _id: 0} }).sort({ name: -1 }).toArray(function(err, result) {
         if (err) throw err;
         console.log(result);
         db.close();
-    });
+    });*/
 
 
 });
+
+
+
