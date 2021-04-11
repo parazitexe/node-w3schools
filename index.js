5bc0a0ef2d48352d19a344f6cda86502c1ce977a
parazitexe <chobak789>
2021-04-12 01:18:57 +0300

    Node.js MongoDB Update https://www.w3schools.com/nodejs/nodejs_mongodb_update.asp

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
 var MongoClient = require('mongodb').MongoClient;
+var http = require('http');
+var fetch = require('node-fetch');
 var url = "mongodb://localhost:27017/";
+
 
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    var dbo = db.db("mydb");
 
-    var filins = db.db("filins");
+    var myFilins = db.db("myFilins");
 
-/*    filins.createCollection("filins-list3", function(err, res) {
-        if (err) throw err;
-        console.log("Collection created!");
-        db.close();
-    });
-    dbo.createCollection("filins3", function(err, res) {
-        if (err) throw err;
-        console.log("Collection created!");
-        db.close();
-    });*/
-
-    dbo.dropCollection("filins", function(err, delOK) {
-        if (err) throw err;
-        if (delOK) console.log("Collection deleted");
-        db.close();
-    })
-
-/*    dbo.collection("customers").deleteMany({ }, function(err, obj) {
-        if (err) throw err;
-        console.log("1 document deleted");
-        db.close();
-    });*/
-
-
-
-/*    dbo.collection("clients").find({ }, { projection: { _id: 0} }).sort({ name: -1 }).toArray(function(err, result) {
-        if (err) throw err;
-        console.log(result);
-        db.close();
-    });*/
-
+    db.close();
 
 });
 
