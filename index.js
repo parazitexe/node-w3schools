756e6a50b70e6b29a4e76718b84415deb95312e2
parazitexe <chobak789>
2021-04-18 01:31:15 +0300

    mongo done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var MongoClient = require('mongodb').MongoClient;
 var http = require('http');
 var fetch = require('node-fetch');
-var url = "mongodb://localhost:27017/";
+//var url = "mongodb://localhost:27017/";
+
 
 
 MongoClient.connect(url, function(err, db) {
@@ -9,11 +10,57 @@ MongoClient.connect(url, function(err, db) {
 
     var myFilins = db.db("myFilins");
 
-    myFilins.collection("filins-user").find({}).limit(2).toArray( function(err, res) {
+
+    myFilins.collection('orders').aggregate([
+        { $lookup:
+                {
+                    from: 'products',
+                    localField: 'product_id',
+                    foreignField: '_id',
+                    as: 'orderdetails'
+                }
+        }
+    ]).toArray(function(err, res) {
+        if (err) throw err;
+        res = res.map((e)=>{
+            return e.orderdetails[0];
+        })
+        console.log(JSON.stringify(res));
+        db.close();
+    });
+
+/*    myFilins.createCollection("orders", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        db.close();
+    })*/
+/*    myFilins.collection('orders').insertOne({ id: 11, product_id: 156, status: 1 },function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        db.close();
+    })*/
+/*    myFilins.createCollection("products", function(err, res) {
         if (err) throw err;
         console.log(res);
         db.close();
     })
+    myFilins.collection('products').insertMany([
+        { _id: 154, name: 'Chocolate Heaven' },
+        { _id: 155, name: 'Tasty Lemons' },
+        { _id: 156, name: 'Vanilla Dreams' }
+    ],function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        db.close();
+    })*/
+
+
+
+/*    myFilins.collection("filins-user").find({}).toArray( function(err, res) {
+        if (err) throw err;
+        console.log(res.length);
+        db.close();
+    })*/
 
     db.close();
 
